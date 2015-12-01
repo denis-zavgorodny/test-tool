41e09003493b61ec28eb9d0ffdfa9b75983cf4ed
Denis Zavgorodny <denis.zavgorodny@gmail.com>
2015-12-01 02:52:36 +0200

    Добавил обработку цепочек действий

--- test-app/toolShoot.js
+++ test-app/toolShoot.js
@@ -1,5 +1,5 @@
  module.exports.toolShoot = {
- 	shoot: function(url, tasks) {
+ 	shoot: function(url, tasks, ev) {
 	 	var fs = require('fs');
 	  	var Spooky = require('spooky');
 	  	var imageDiff = require('image-diff');
@@ -12,69 +12,68 @@
 				casper: {
 					logLevel: 'debug',
 					viewportSize: {
-						width: 800, 
-						height: 600
+						width: 1280, 
+						height: 900
 					}
 		            // verbose: true
 				}
 			}, function (err) {
-			  	imageDiff({
-					actualImage: 'weather.png',
-					expectedImage: 'weather1.png',
-					diffImage: 'weather.diff.png',
-					shadow: true
-				}, function (err, imagesAreSame) {
-				  // error will be any errors that occurred 
-				  // imagesAreSame is a boolean whether the images were the same or not 
-				  // diffImage will have an image which highlights differences 
-				});
+			 //  	imageDiff({
+				// 	actualImage: 'weather.png',
+				// 	expectedImage: 'weather1.png',
+				// 	diffImage: 'weather.diff.png',
+				// 	shadow: true
+				// }, function (err, imagesAreSame) {
+				//   // error will be any errors that occurred 
+				//   // imagesAreSame is a boolean whether the images were the same or not 
+				//   // diffImage will have an image which highlights differences 
+				// });
 				// spooky_instance.start(url, function(){
 					
 				// });
 				spooky_instance.start(url, function(){
 				});
 				
-				taskIteration(spooky_instance, tasks, 0);
-		        function taskIteration(spooky_instance, tasks, index) {
-		        	console.log(tasks.length, index);
-		        	if(tasks.length <= index)
+				taskIteration(tasks, 0);
+		        function taskIteration(tasks, index) {
+		        	if(typeof tasks == 'undefined' || tasks.length <= index)
 		        		return;
 		        	task = tasks[index];
-		        	console.log(task);
 		        	index++;
+		        		console.log(task);
 		            
 		            	switch(task.type) {
 							case "screenShoot":
 								var file_name = task.selector.toString().replace('.','').replace('#','') + '.png';
-								spooky_instance.then([{file_name: file_name, selector: task.selector}, function(){
-									if (this.exists(selector)) {
-								        this.captureSelector(file_name, selector);
-	                                	this.emit('onShootSuccess', {message: selector});
-								    } else {
-								    	this.emit('onShootError', 'Not found selector' + selector);
-								    }
-									
-						            // taskIteration(spooky_instance, tasks, index);
-					            }]);		
+								spooky_instance.then([
+									{
+										file_name: file_name, 
+										selector: task.selector,
+										index: index
+									}, 
+									function(){
+										if (this.exists(selector)) {
+									        this.captureSelector(file_name, selector);
+		                                	this.emit('onShootSuccess', {message: selector, next: index});
+		                                	this.emit('onShootNext', index);
+									    } else {
+									    	this.emit('onShootError', 'Not found selector' + selector);
+									    }
+						            }
+					            ]);		
 					            
 								break;
 							default:
 								break;	
 						}
-			            
-			            
-			        
+			        taskIteration(tasks, index);   
 		        }		
-				
-
-				
-				
-		        
-
 		        spooky_instance.run();
 			});
 		spooky_instance.on('onShootSuccess', function (message) {
 		    console.log(message);
+		    console.log('============================================');
+		    ev.emit('responseWS', message);
 		});
 		spooky_instance.on('onShootError', function (message) {
 		    console.log(message);
--- test-app/ws.js
+++ test-app/ws.js
@@ -1,5 +1,7 @@
 var WebSocketServer = require('ws').Server, 
 	wss = new WebSocketServer({ port: 8080 });
+var EventEmitter = require('events').EventEmitter;
+var ev = new EventEmitter();
 
 wss.on('connection', function connection(ws) {
 	var toolShoot = require('./toolShoot');
@@ -10,9 +12,14 @@ wss.on('connection', function connection(ws) {
 		if(typeof data.tasks != 'undefined') {
 		    data.tasks.forEach(function(task, key, ar){
 		    	// console.log(task);
-				toolShoot.toolShoot.shoot(task.url, task.actions);
+				toolShoot.toolShoot.shoot(task.url, task.actions, ev);
 			});	
 		}
+		
+	});
+	ev.on('responseWS', function(message){
+		console.log('from socket');
+		console.log(message);
 		ws.send(message);	
 	});
 
